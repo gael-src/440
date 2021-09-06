d2ba8a2c74ae9ce9e9e2015920c48b83b3eeef6d
deyuhub <yu.de-gen@gmx.de>
2021-09-06 14:00:11 +0200

    add build script

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
 // import dependencies
 
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
 // A simple react component
 const App = () => {
+  const age = 12;
+  const output = <span> James is {age} years old </span>;
+  const customer = {
+    first_name: "Bob",
+    last_name: "Dylan",
+  };
 
-const age =12;
-const output = <span> James is { age } years old </span>;
-const customer = {
-    first_name: 'Bob',
-    last_name: 'Dylan'
+  return (
+    <React.Fragment>
+      <h1> Hello World </h1>
+      <p>{output}</p>
+      <p>{customer.first_name}</p>
+    </React.Fragment>
+  );
 };
 
-
-    return (
-        <React.Fragment>
-         <h1> Hello World </h1>
-         <p>{output}</p>
-         <p>{customer.first_name}</p>
-        </React.Fragment>
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById("root")) // render element on div with ID of 'root'
+ReactDOM.render(<App />, document.getElementById("root")); // render element on div with ID of 'root'
